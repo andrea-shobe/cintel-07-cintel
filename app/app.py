3588070ad664977fe624bcfa38e3b43138e09874
Andrea Shobe <20andrea03@gmail.com>
2024-12-02 10:47:37 -0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,38 +1,49 @@
+# define imports
 import seaborn as sns
 from faicons import icon_svg
-
 from shiny import reactive
 from shiny.express import input, render, ui
-import palmerpenguins 
+import palmerpenguins
+from shinyswatch import theme  # import shinyswatch to use bootswatch themes
 
+# Define Dataframe
 df = palmerpenguins.load_penguins()
 
-ui.page_opts(title="Penguins dashboard", fillable=True)
+
+# Page Title
+ui.page_opts(title="Penguins dashboard", fillable=True, theme=theme.minty)
 
 
+# Define Sidebar title
 with ui.sidebar(title="Filter controls"):
+
+    # input slider to define penguin mass
     ui.input_slider("mass", "Mass", 2000, 6000, 6000)
+
+    # input checkbox to select which specieis to display in outputs
     ui.input_checkbox_group(
         "species",
         "Species",
         ["Adelie", "Gentoo", "Chinstrap"],
         selected=["Adelie", "Gentoo", "Chinstrap"],
     )
+
+    # Sidebar Links
     ui.hr()
     ui.h6("Links")
     ui.a(
         "GitHub Source",
-        href="https://github.com/denisecase/cintel-07-tdash",
+        href="https://github.com/andrea-shobe/cintel-07-cintel",
         target="_blank",
     )
     ui.a(
         "GitHub App",
-        href="https://denisecase.github.io/cintel-07-tdash/",
+        href="https://andrea-shobe.github.io/cintel-07-cintel/",
         target="_blank",
     )
     ui.a(
         "GitHub Issues",
-        href="https://github.com/denisecase/cintel-07-tdash/issues",
+        href="https://github.com/andrea-shobe/cintel-07-cintel/issues",
         target="_blank",
     )
     ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank")
@@ -47,23 +58,29 @@ with ui.sidebar(title="Filter controls"):
         target="_blank",
     )
 
-
+# -----------------------------------------
+# Beginning of main page content
+# -----------------------------------------
 with ui.layout_column_wrap(fill=False):
-    with ui.value_box(showcase=icon_svg("earlybirds")):
+
+    # Output of number of penguins based off of species selected
+    with ui.value_box(showcase=icon_svg("earlybirds"),style="color:#56cc9d;"):
         "Number of penguins"
 
         @render.text
         def count():
             return filtered_df().shape[0]
 
-    with ui.value_box(showcase=icon_svg("ruler-horizontal")):
+    # output of average bill length
+    with ui.value_box(showcase=icon_svg("ruler-horizontal"), style="color: #fd7e14;"):
         "Average bill length"
 
         @render.text
         def bill_length():
             return f"{filtered_df()['bill_length_mm'].mean():.1f} mm"
 
-    with ui.value_box(showcase=icon_svg("ruler-vertical")):
+    # output of average bill depth
+    with ui.value_box(showcase=icon_svg("ruler-vertical"), style="color: #007bff;"):
         "Average bill depth"
 
         @render.text
@@ -72,8 +89,13 @@ with ui.layout_column_wrap(fill=False):
 
 
 with ui.layout_columns():
+
+    # Bill Length and Depth scatterpolot based off of species selected-------------
     with ui.card(full_screen=True):
-        ui.card_header("Bill length and depth")
+        ui.card_header(
+            "Bill length and depth",
+            style="font-weight: bold; color: #56cc9d;",
+        )
 
         @render.plot
         def length_depth():
@@ -84,8 +106,12 @@ with ui.layout_columns():
                 hue="species",
             )
 
+    # Filtered DataGrid based off of selected species----------------------------
     with ui.card(full_screen=True):
-        ui.card_header("Penguin data")
+        ui.card_header(
+            "Penguin data",
+            style="font-weight: bold; color: #56cc9d;",
+        )
 
         @render.data_frame
         def summary_statistics():
@@ -99,9 +125,10 @@ with ui.layout_columns():
             return render.DataGrid(filtered_df()[cols], filters=True)
 
 
-#ui.include_css(app_dir / "styles.css")
+# ui.include_css(app_dir / "styles.css")
 
 
+# define reactive calc for filtered df
 @reactive.calc
 def filtered_df():
     filt_df = df[df["species"].isin(input.species())]
